4a3617daf2d6098029f40cf1ae9de01fc3264839
TruongHidu <nguyenvantruong1009204@gmail.com>
2025-02-14 08:14:57 +0700

    Initial commit

---
+++ src/test/java/com/Springboottest/SpringBootTest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Springboottest.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
